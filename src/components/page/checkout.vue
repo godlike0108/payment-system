8ae86b52462bf6fec3dfad2d0c69ffd135c23cb4
yoyo <po8867@gmail.com>
2018-06-13 14:54:12 +0800

    Modify moment

--- src/components/page/checkout.vue
+++ src/components/page/checkout.vue
@@ -102,7 +102,7 @@ export default {
                     {
                         title: '最後更新時間',
                         key: 'updated_at',
-                        minWidth:100
+                        minWidth:150
                     }
                 ],
         formInline: {
@@ -180,9 +180,9 @@ export default {
              }
 
              if (item.created_at){
-                 item.created_at = this.$moment(item.created_at)
-                 .tz('Asia/Taipei')
-                 .format('YYYY-MM-DD HH:mm:ss');
+                 item.created_at = this.$moment()
+                        .tz(item.created_at, 'Asia/Taipei')
+                        .format('YYYY-MM-DD HH:mm:ss')
              }
              return item
           })
--- src/components/page/userIndex.vue
+++ src/components/page/userIndex.vue
@@ -147,9 +147,9 @@ export default {
              }
 
              if(item.created_at) {
-                 item.created_at = this.$moment(item.created_at)
-                 .tz('Asia/Taipei')
-                 .format('YYYY-MM-DD HH:mm:ss');
+                 item.created_at = this.$moment()
+                        .tz(item.created_at, 'Asia/Taipei')
+                        .format('YYYY-MM-DD HH:mm:ss')
              }
                 // console.log(item)
              
